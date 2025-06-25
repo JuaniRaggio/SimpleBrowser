5a896203e4bcbc2bbed3f7a2077cc3e1888f7554
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-06-25 19:17:07 -0300

    Version 4: Web rendering

--- src/main/java/com/JuaniRaggio/Browser/MyBrowser.java
+++ src/main/java/com/JuaniRaggio/Browser/MyBrowser.java
@@ -4,12 +4,16 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
  
 /**
- * Paso 3: Interactuar con el campo de texto usando EventHandler
+ * Paso 4: Agregar un campo WebView y cargar el sitio deseado.
  */
 public class MyBrowser extends Application {
+ 
+    //Componente WebView para mostrar sitios web
+    private WebView webView = new WebView();
  
     public static void main(String[] args) {
         launch(args);
@@ -18,19 +22,13 @@ public class MyBrowser extends Application {
     @Override
     public void start(Stage primaryStage) {
         VBox vBox = new VBox();
- 
-        //El textfield empieza con un texto inicial.
         TextField textField = new TextField("http://");
- 
         /**
-         * Agregamos un EventHandler para que ante cualquier evento
-         * del campo de texto se ejecute el siguiente código
-         * Ante algún evento en el campo de texto
-         * se imprime en salida estándar el texto del campo.
+         * Cambiamos el EventHandler para que ante un evento
+         * en el campo de texto, se cargue el sitio web ingresado
          */
-        textField.setOnAction(event -> System.out.println(textField.getText()));
- 
-        vBox.getChildren().add(textField);
+        textField.setOnAction(event -> webView.getEngine().load(textField.getText()));
+        vBox.getChildren().addAll(textField, webView);
         Scene scene = new Scene(vBox, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
